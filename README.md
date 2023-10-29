d3128c2d17bda0327cdd80feca9576c53755409a
Wesley de Groot <email@wesleydegroot.nl>
2023-10-29 15:04:12 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
-# Hello World Extension
+# Aurora Editor Test Extension
+
+> **Warning**\
+> This repo is for testing purposes only.\
+> Please do not use this repo as a template for your own extension.\
+> Use [aurora-extension-template](https://github.com/AuroraEditor/Extension-HelloWorld).
 
 <a href='https://twitter.com/intent/tweet?text=Try%20this%20new%20open-source%20code%20editor,%20Aurora%20Editor&url=https://auroraeditor.com&via=Aurora_Editor&hashtags=AuroraEditor,editor,AEIDE,developers,Aurora,OSS' target='_blank'><img src='https://img.shields.io/twitter/url/http/shields.io.svg?style=social'></a>  <a href='https://chat.whatsapp.com/IOoaSbj7Km4BH3k5wlJPx0' target='_blank'>
     <img alt="Discord" src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" height='28px' width='28px'>
   </a>
 
-Sample `Hello World` extension.
-
-Please report issues on https://github.com/AuroraEditor/AuroraEditor
+Sample extension.
 
 For help about extensions go to [#extensions](https://discord.gg/cCcwRFfY8f) on our [Discord](https://discord.gg/QYTtDYMMYj) server.
 
---
+++ Test.swift
@@ -0,0 +1,103 @@
+//
+//  Test.swift
+//  TestExtension
+//
+//  Created by Wesley de Groot on 6 October 2022.
+//
+
+import Foundation
+import AEExtensionKit
+import SwiftUI
+
+public class TestExtension: ExtensionInterface {
+    var api: ExtensionAPI
+    var AuroraAPI: AuroraAPI = { _, _ in }
+
+    init(api: ExtensionAPI) {
+        self.api = api
+        print("Hello from TestExtension: \(api)!")
+    }
+
+    public func register() -> ExtensionManifest {
+        return .init(
+            name: "TestExtension",
+            displayName: "TestExtension",
+            version: "1.0",
+            minAEVersion: "1.0"
+        )
+    }
+
+    public func respond(action: String, parameters: [String: Any]) -> Bool {
+        print("respond(action: String, parameters: [String: Any])", action, parameters)
+
+        if action == "registerCallback" {
+            if let api = parameters["callback"] as? AuroraAPI {
+                AuroraAPI = api
+            }
+
+            for i in 0...9 {
+                DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+
+                    if i == 5 {
+                        self.AuroraAPI(
+                            "showWarning",
+                            ["message": "a wild warning encountered!"]
+                        )
+                    }
+
+                    print("Waiting for \(10 - i) seconds...")
+
+                    self.AuroraAPI(
+                        "showNotification",
+                        [
+                            "title":"showNotification",
+                            "message": "Waiting for \(10 - i) seconds..."
+                        ]
+                    )
+                }
+            }
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+//                self.AuroraAPI(
+//                    "openWindow",
+//                    ["view": myWindow()]
+//                )
+                self.AuroraAPI(
+                    "openSettings",
+                    [:]
+                )
+                self.AuroraAPI(
+                    "showError",
+                    ["message": "Ok, settings should be opened right now?"]
+                )
+            }
+        }
+
+
+        return true
+    }
+}
+
+struct myWindow: View {
+    var body: some View {
+        VStack {
+            Text("Hello, from extension")
+            Divider()
+            Text("SwiftUI View.")
+            Divider()
+            Button {
+                let _ = print("DID CLICK BUTTON")
+            } label: {
+                Text("Button")
+            }
+
+        }
+    }
+}
+
+@objc(TestBuilder)
+public class TestBuilder: ExtensionBuilder {
+    public override func build(withAPI api: ExtensionAPI) -> ExtensionInterface {
+        return TestExtension(api: api)
+    }
+}
